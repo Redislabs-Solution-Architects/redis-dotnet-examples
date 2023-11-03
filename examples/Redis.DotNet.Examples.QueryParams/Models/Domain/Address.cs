2a0aa9a6d8c2c0610d8390f82ff427543b900ae2
roberto.perez <robertopc@gmail.com>
2023-11-03 16:20:31 -0400

    adding search

---
+++ examples/Redis.DotNet.Examples.QueryParams/Models/Domain/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Redis.DotNet.Examples.QueryParams.Models.Domain
+{
+    public class Address
+    {
+        public string? AddressLine1 { get; set; }
+
+        public string? AddressLine2 { get; set; }
+        public string? City { get; set; }
+
+        public Address(string addressLine1, string? city)
+        {
+            AddressLine1 = addressLine1;
+            City = city;
+        }
+    }
+}
+
---
+++ examples/Redis.DotNet.Examples.QueryParams/Models/Domain/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Redis.DotNet.Examples.QueryParams.Models.Contracts;
+using Redis.OM.Modeling;
+
+namespace Redis.DotNet.Examples.QueryParams.Models.Domain
+{
+    [Document(StorageType = StorageType.Json)]
+    public class Customer
+    {
+        [RedisIdField][Indexed] public Guid Id { get; set; }
+
+        [Indexed] public string FullName { get; set; } = null!;
+        [Indexed] public string Email { get; set; } = null!;
+
+        [Indexed] public string[] Publications { get; set; } = null!;
+
+        [Indexed] public Address? Address { get; set; }
+    }
+}
+
